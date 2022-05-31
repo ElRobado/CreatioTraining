0e4deeb258f7fd890cac216397fe3ea79ef04c88
ElRobado <AndreasFurto@gmail.com>
2022-05-31 16:02:24 +0300

    feature/CT-3
    1. Создан и наполнен справочник "Состояния выпуска", добавлены соответствующие привязки.
    
    2. Создана деталь "Выпуски" со страницей редактирования, реализован редактируемый реестр.
    
    3. Деталь "Выпуски" добавлена на страницу редактирования раздела "Рекламный блок".

---
+++ Schemas/DomSessionDetail/DomSessionDetail.js
@@ -0,0 +1,106 @@
+define("DomSessionDetail", ["ConfigurationGrid", "ConfigurationGridGenerator",
+    "ConfigurationGridUtilities"],
+    function (ConfigurationGrid) {
+        return {
+            entitySchemaName: "DomSession",
+            attributes: {
+                "IsEditable": {
+                    dataValueType: Terrasoft.DataValueType.BOOLEAN,
+                    type: Terrasoft.ViewModelColumnType.VIRTUAL_COLUMN,
+                    value: true
+                }
+            },
+            diff: /**SCHEMA_DIFF*/[{
+                "operation": "merge",
+                "name": "DataGrid",
+                "values": {
+                    "className": "Terrasoft.ConfigurationGrid",
+                    "generator": "ConfigurationGridGenerator.generatePartial",
+                    "generateControlsConfig": {
+                        "bindTo": "generateActiveRowControlsConfig"
+                    },
+                    "changeRow": {
+                        "bindTo": "changeRow"
+                    },
+                    "unSelectRow": {
+                        "bindTo": "unSelectRow"
+                    },
+                    "onGridClick": {
+                        "bindTo": "onGridClick"
+                    },
+                    "activeRowActions": [
+                        {
+                            "className": "Terrasoft.Button",
+                            "style": this.Terrasoft.controls.ButtonEnums.style.TRANSPARENT,
+                            "tag": "card",
+                            "markerValue": "card",
+                            "imageConfig": {
+   								"bindTo": "Resources.Images.CardIcon"
+                            }
+                        },
+                        {
+                            "className": "Terrasoft.Button",
+                            "style": this.Terrasoft.controls.ButtonEnums.style.TRANSPARENT,
+                            "tag": "save",
+                            "markerValue": "save",
+                            "imageConfig": {
+                                "bindTo": "Resources.Images.SaveIcon"
+                            }
+                        },
+                        {
+                            "className": "Terrasoft.Button",
+                            "style": this.Terrasoft.controls.ButtonEnums.style.TRANSPARENT,
+                            "tag": "cancel",
+                            "markerValue": "cancel",
+                            "imageConfig": {
+                                "bindTo": "Resources.Images.CancelIcon"
+                            }
+                        },
+                        {
+                            "className": "Terrasoft.Button",
+                            "style": this.Terrasoft.controls.ButtonEnums.style.TRANSPARENT,
+                            "tag": "remove",
+                            "markerValue": "remove",
+                            "imageConfig": {
+                                "bindTo": "Resources.Images.RemoveIcon"
+                            }
+                        }
+                    ],
+                    "initActiveRowKeyMap": {
+                        "bindTo": "initActiveRowKeyMap"
+                    },
+                    "activeRowAction": {
+                        "bindTo": "onActiveRowAction"
+                    },
+                    "multiSelect": {
+                        "bindTo": "MultiSelect"
+                    }
+                }
+            }],
+            /**SCHEMA_DIFF*/
+            mixins: {
+                ConfigurationGridUtilities: "Terrasoft.ConfigurationGridUtilities"
+            },
+            methods: {
+              onActiveRowAction: function(buttonTag, primaryColumnValue) {
+                  this.set("LastActiveRow", primaryColumnValue);
+                  switch (buttonTag) {
+                      case "card":
+                          this.editCurrentRecord();
+                          break;
+                      case "remove":
+                          this.deleteRecords();
+                          break;
+                      case "cancel":
+                          this.discardChanges(primaryColumnValue);
+                          break;
+                      case "save":
+                          this.onActiveRowSave(primaryColumnValue);
+                          break;
+                      default:
+                          break;
+                  }
+              }
+           }
+        };
+    });
---
+++ Schemas/DomSessionPage/DomSessionPage.js
@@ -0,0 +1,98 @@
+define("DomSessionPage", [], function() {
+	return {
+		entitySchemaName: "DomSession",
+		attributes: {},
+		modules: /**SCHEMA_MODULES*/{}/**SCHEMA_MODULES*/,
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		businessRules: /**SCHEMA_BUSINESS_RULES*/{}/**SCHEMA_BUSINESS_RULES*/,
+		methods: {},
+		dataModels: /**SCHEMA_DATA_MODELS*/{}/**SCHEMA_DATA_MODELS*/,
+		diff: /**SCHEMA_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "DomDate",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "DomDate"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "DomDuration",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 0,
+						"layoutName": "Header"
+					},
+					"bindTo": "DomDuration"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "DomSessionState",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "DomSessionState"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 2
+			},
+			{
+				"operation": "insert",
+				"name": "DomHost",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 12,
+						"row": 1,
+						"layoutName": "Header"
+					},
+					"bindTo": "DomHost"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 3
+			},
+			{
+				"operation": "insert",
+				"name": "DomPrice",
+				"values": {
+					"layout": {
+						"colSpan": 12,
+						"rowSpan": 1,
+						"column": 0,
+						"row": 2,
+						"layoutName": "Header"
+					},
+					"bindTo": "DomPrice"
+				},
+				"parentName": "Header",
+				"propertyName": "items",
+				"index": 4
+			}
+		]/**SCHEMA_DIFF*/
+	};
+});
